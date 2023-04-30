a143837f7816a72c42224f30e86828e0a83280c2
Asmaul-Shahana <jucse28.360@gmail.com>
2023-04-30 21:18:46 +0600

    another commit

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,22 @@
 import React from 'react';
+import './Cart.css';
 
 const Cart = ({ cart, handleRemoveFromCart }) => {
     return (
         <div>
             <h2>Order Summary: {cart.length}</h2>
+            <p className={`bold bordered ${cart.length >=2 ? 'violet' : 'sky' }`}>Get your tshirt now</p>
+
+            { cart.length> 2 ? <span className='orange'>Voucher applicable</span> : <span>Voucher not applicable</span> }
+            
             {
                 cart.map(tshirt => <p key={tshirt._id}>
                     {tshirt.name}
                     <button onClick={() => handleRemoveFromCart(tshirt._id)}>X</button></p>
                 )
             }
+            { cart.length === 5 ? <span className='yellow'>Special 40% offer
+            </span> : '' }
         </div>
     );
 };
